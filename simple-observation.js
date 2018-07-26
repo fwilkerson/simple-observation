efeb180b4f7dda3745a80d193660cffbcc84accf
Frank A. Wilkerson <frank.wilkerson@outlook.com>
2018-07-25 20:29:07 -0700

    add some comments

--- simple-observation.js
+++ simple-observation.js
@@ -2,6 +2,7 @@ import test from "japa";
 import { createStore } from "redux";
 import { spy } from "sinon";
 
+// simplistic reducer for our demo
 function counter(state = 0, { type, payload }) {
   switch (type) {
     case "TEST":
@@ -11,8 +12,15 @@ function counter(state = 0, { type, payload }) {
   }
 }
 
+// we'll wedge this in to prove the unsubscribe works
 const sanityCheck = spy();
 
+/**
+ * The simplest of observations would be a waiting period
+ * determined by the pending promise. A sucess condition
+ * determined by the .then and a failure condition determined
+ * by the .catch.
+ */
 function simpleObservation({ getState, subscribe }) {
   return new Promise((res, rej) => {
     const unsubscribe = subscribe(() => {
@@ -31,6 +39,7 @@ function simpleObservation({ getState, subscribe }) {
   });
 }
 
+// helper function to free up the event loop
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 test.group("simple observation", group => {
@@ -43,14 +52,16 @@ test.group("simple observation", group => {
 
     store = createStore(counter);
 
+    // set to the loading state then call our function
     simpleObservation(store)
-      .then(resolve)
-      .catch(reject);
+      .then(resolve) // handle the sucess case
+      .catch(reject); // handle the failure case
   });
 
   test("should resolve if count === 3", async assert => {
     store.dispatch({ type: "TEST", payload: 3 });
 
+    // release the kraken
     await sleep();
 
     assert.equal(resolve.called, true);
@@ -64,6 +75,7 @@ test.group("simple observation", group => {
   test("should reject if count < 0", async assert => {
     store.dispatch({ type: "TEST", payload: -1 });
 
+    // release the kraken
     await sleep();
 
     assert.equal(reject.called, true);
